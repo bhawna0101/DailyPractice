9f266f74d8a1ac2c4a674314a82d524a1c3724fc
bhawnaarora <bhawna.arora@dineout.co.in>
2021-04-23 14:01:24 +0530

    abstract and interfaces

---
+++ src/com/interview/D1.java
@@ -0,0 +1,26 @@
+package com.interview;
+
+public abstract class D1 implements D1Interface{
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void method1() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void method2() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	
+
+
+
+}
---
+++ src/com/interview/D1Interface.java
@@ -0,0 +1,19 @@
+package com.interview;
+
+public interface D1Interface {
+	
+	public int a;
+	
+	public void method1();
+	
+	public void method2();
+	
+	public void method3();
+	
+//	public void method4()
+//	{
+//		System.out.println("hey"); 		//compile time error abs methods do not specify a body
+//	}
+	
+
+}
